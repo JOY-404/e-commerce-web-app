7cd72bfcce3b001d263091660bb01b43c12ec775
Janmejoy Sahoo <b416025@iiit-bh.ac.in>
2022-04-12 14:53:10 +0530

    listing of products dynamically from backend added

---
+++ js/server.js
@@ -0,0 +1,28 @@
+
+// Add products from server
+document.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/products').then(res => {
+        res.data.forEach(product => {
+            const prodElement = document.createElement('article');
+            prodElement.className = 'card product-item';
+            prodElement.setAttribute('id', `item${product.id}`);
+            prodElement.innerHTML = `<header class="card__header">
+                    <h1 class="product__title">
+                        ${product.title}
+                    </h1>
+                </header>
+                <div class="card__image">
+                    <img src="${product.imageUrl}" alt="${product.title}">
+                </div>
+                <div class="card__content">
+                    <h2 class="product__price"><span>₹ <span>${product.price}</span></span>
+                    </h2>
+                </div>
+                <div class="card__actions">
+                    <a href="" class="btn btn-detail">Details</a>
+                    <button class="btn btn-cart">Add to Cart</button>
+                </div>`;
+            gridItems.appendChild(prodElement);
+        });
+    });
+});
