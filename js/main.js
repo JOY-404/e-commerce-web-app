372710f008a30bdbec25172b9097e6474790e741
Janmejoy Sahoo <b416025@iiit-bh.ac.in>
2022-04-12 18:33:27 +0530

    cart loading from DB, remove from DB added

--- js/main.js
+++ js/main.js
@@ -47,25 +47,55 @@ gridItems.addEventListener('click', (e) => {
   if(e.target.className == 'btn btn-cart') {
     const id = e.target.parentNode.parentNode.id;
     const pid = document.querySelector(`#${id} .card__actions input`).value;
-    const pName = document.querySelector(`#${id} header h1`).innerText;
-    const price = document.querySelector(`#${id} .card__content .product__price span span`).innerText;
-    const imgSrc = document.querySelector(`#${id} .card__image img`).src;
 
     // POST request to DB with pid
     axios.post('http://localhost:3000/cart', { productId: pid }).then(res => {
-
-      if (document.querySelector(`#cart-${id}`)) {
-        const cartItem = document.querySelector(`#cart-${id}`);
-        const cartQty = parseInt(cartItem.lastElementChild.firstElementChild.value) + 1;
-        cartItem.lastElementChild.firstElementChild.value = cartQty;
-        //console.log((cartQty * parseFloat(price)).toFixed(2));
-        cartItem.querySelector('.cart-price.cart-col span').innerText = `${(cartQty * parseFloat(price)).toFixed(2)}`;
+      saveToCartDOM(id);
+    }).catch(err => {
+      if (err.response) {
+        showNotification(`Oops! Something went wrong! ${err.response.status}`, true);
+      }
+      else if (err.request) {
+        showNotification('Error: No Response From Server', true);
       }
       else {
-        const cartItem = document.createElement('div');
-        cartItem.className = 'cart-row';
-        cartItem.setAttribute('id', `cart-${id}`);
-        cartItem.innerHTML = `<span class='cart-item cart-col'>
+        showNotification(err.message, true);
+      }
+    });
+  }
+});
+
+function showNotification(message, isError) {
+  // Gives a short toast
+  const notification = document.getElementById('notification');
+  const notice = document.createElement('div');
+  notice.classList.add('toast');
+  if (isError === true)
+    notice.innerHTML = `<i class="fa fa-exclamation-circle fa-lg" aria-hidden="true" style='color: red;'></i>&nbsp;&nbsp;${message}`;
+  else
+    notice.innerHTML = `<i class="fa fa-check-circle fa-lg" aria-hidden="true" style='color: #99C24D;'></i>&nbsp;&nbsp;${message}`;
+
+  notification.insertBefore(notice, notification.firstChild);
+  setTimeout(() => notice.remove(), 3000);
+}
+
+function saveToCartDOM(id) {
+  const pName = document.querySelector(`#${id} header h1`).innerText;
+  const price = document.querySelector(`#${id} .card__content .product__price span span`).innerText;
+  const imgSrc = document.querySelector(`#${id} .card__image img`).src;
+
+  if (document.querySelector(`#cart-${id}`)) {
+    const cartItem = document.querySelector(`#cart-${id}`);
+    const cartQty = parseInt(cartItem.lastElementChild.firstElementChild.value) + 1;
+    cartItem.lastElementChild.firstElementChild.value = cartQty;
+    //console.log((cartQty * parseFloat(price)).toFixed(2));
+    cartItem.querySelector('.cart-price.cart-col span').innerText = `${(cartQty * parseFloat(price)).toFixed(2)}`;
+  }
+  else {
+    const cartItem = document.createElement('div');
+    cartItem.className = 'cart-row';
+    cartItem.setAttribute('id', `cart-${id}`);
+    cartItem.innerHTML = `<span class='cart-item cart-col'>
                             <img class='cart-img' src="${imgSrc}" alt="">
                             <span>${pName}</span>
                         </span>
@@ -74,41 +104,19 @@ gridItems.addEventListener('click', (e) => {
                             <input type="text" value='1' readonly>
                             <button class="btn btn-danger">REMOVE</button>
                         </span>`;
-        const cartItems = document.querySelector('.cart-items');
-        cartItems.appendChild(cartItem);
-      }
-
-      // Update Cart Total Amount
-      let cartTotPrice = document.getElementById('total-value').innerText;
-      cartTotPrice = (parseFloat(price) + parseFloat(cartTotPrice)).toFixed(2);
-      document.getElementById('total-value').innerText = cartTotPrice;
-
-      // Update cart qty count
-      document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) + 1;
-
-      // Gives a short toast
-      const notification = document.getElementById('notification');
-      const notice = document.createElement('div');
-      notice.classList.add('toast');
-      notice.innerHTML = `<i class="fa fa-check-circle fa-lg" aria-hidden="true" style='color: #99C24D;'></i>&nbsp;&nbsp;${pName} Added to Cart`;
-      //notification.appendChild(notice);
-      notification.insertBefore(notice, notification.firstChild);
-      setTimeout(() => notice.remove(), 3000);
-
-    }).catch(err => {
-      console.log(err);
-      showErrorNotification();
-    });
+    const cartItems = document.querySelector('.cart-items');
+    cartItems.appendChild(cartItem);
   }
-});
 
-function showErrorNotification() {
-  const notification = document.getElementById('notification');
-  const notice = document.createElement('div');
-  notice.classList.add('toast');
-  notice.innerHTML = `<i class="fa fa-exclamation-circle fa-lg" aria-hidden="true" style='color: red;'></i>&nbsp;&nbsp;Oops! Something Went Wrong`;
-  notification.insertBefore(notice, notification.firstChild);
-  setTimeout(() => notice.remove(), 3000);
+  // Update Cart Total Amount
+  let cartTotPrice = document.getElementById('total-value').innerText;
+  cartTotPrice = (parseFloat(price) + parseFloat(cartTotPrice)).toFixed(2);
+  document.getElementById('total-value').innerText = cartTotPrice;
+
+  // Update cart qty count
+  document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) + 1;
+  
+  showNotification(`${pName} Added to Cart`, false);
 }
 
 // Remove Items from Cart function
@@ -116,16 +124,31 @@ const cartItems = document.querySelector('.cart-items');
 cartItems.addEventListener('click', (e) => {
   //console.log(e.target.className);
   if (e.target.className == 'btn btn-danger') {
-    // Remove that item from the list
     const cartItem = e.target.parentNode.parentNode;
-    const itemAmt = parseFloat(cartItem.querySelector(`.cart-price span`).innerText);
-    const itemQty = parseInt(cartItem.querySelector('.cart-qty input').value);
-    cartItems.removeChild(cartItem);
-    // Update Cart Total Amount
-    let cartTotPrice = document.getElementById('total-value').innerText;
-    cartTotPrice = (parseFloat(cartTotPrice) - itemAmt).toFixed(2);
-    document.getElementById('total-value').innerText = cartTotPrice;
-    // Update cart qty count
-    document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) - itemQty;
+    const pid = cartItem.id.substring(9);
+
+    // POST request to DB with pid
+    axios.post('http://localhost:3000/cart-delete-item', { productId: pid }).then(res => {
+      // Remove that item from the list
+      const itemAmt = parseFloat(cartItem.querySelector(`.cart-price span`).innerText);
+      const itemQty = parseInt(cartItem.querySelector('.cart-qty input').value);
+      cartItems.removeChild(cartItem);
+      // Update Cart Total Amount
+      let cartTotPrice = document.getElementById('total-value').innerText;
+      cartTotPrice = (parseFloat(cartTotPrice) - itemAmt).toFixed(2);
+      document.getElementById('total-value').innerText = cartTotPrice;
+      // Update cart qty count
+      document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) - itemQty;
+    }).catch(err => {
+      if (err.response) {
+        showNotification(`Oops! Something went wrong! ${err.response.status}`, true);
+      }
+      else if (err.request) {
+        showNotification('Error: No Response From Server', true);
+      }
+      else {
+        showNotification(err.message, true);
+      }
+    });
   }
 });
--- js/server.js
+++ js/server.js
@@ -21,10 +21,60 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="card__actions">
                     <!-- <a href="" class="btn btn-detail">Details</a> -->
                     <button class="btn btn-cart">Add to Cart</button>
-                    <input type="hidden" name="productId" value="${product.id}">
+                    <input type="hidden" value="${product.id}">
                 </div>`;
             gridItems.appendChild(prodElement);
         });
+    }).catch(err => {
+        if (err.response) {
+            showNotification(`Oops! Something went wrong! ${err.response.status}`, true);
+        }
+        else if (err.request) {
+            showNotification('Error: No Response From Server', true);
+        }
+        else {
+            showNotification(err.message, true);
+        }
     });
+
+    axios.get('http://localhost:3000/cart').then(res => {
+        let cartTotPrice = 0;
+        let totalQty = 0;
+
+        res.data.forEach(product => {
+            const cartItemElement = document.createElement('div');
+            cartItemElement.className = 'cart-row';
+            cartItemElement.setAttribute('id', `cart-item${product.id}`);
+            const totAmt = parseFloat(product.price) * parseFloat(product.cartItems.quantity);
+            cartItemElement.innerHTML = `<span class='cart-item cart-col'>
+                            <img class='cart-img' src="${product.imageUrl}" alt="">
+                            <span>${product.title}</span>
+                        </span>
+                        <span class='cart-price cart-col'>₹&nbsp;<span>${totAmt.toFixed(2)}</span></span>
+                        <span class='cart-qty cart-col'>
+                            <input type="text" value='${product.cartItems.quantity}' readonly>
+                            <button class="btn btn-danger">REMOVE</button>
+                        </span>`;
+            cartItems.appendChild(cartItemElement);
+
+            cartTotPrice += totAmt;
+            totalQty += parseInt(product.cartItems.quantity);
+        });
+
+        // Update Cart Total Amount
+        document.getElementById('total-value').innerText = cartTotPrice.toFixed(2);;
+        // Update cart qty count
+        document.querySelector('.cart-number').innerText = totalQty;
+    }).catch(err => {
+        if (err.response) {
+            showNotification(`Oops! Something went wrong! ${err.response.status}`, true);
+        }
+        else if (err.request) {
+            showNotification('Error: No Response From Server', true);
+        }
+        else {
+            showNotification(err.message, true);
+        }
+    });    
 });
 
